a897475b47e0010b43c551b9cd0db20656337953
Tony <stephen.gionfriddo@gmail.com>
2020-02-26 16:59:23 -0700

    db create and find actions added

--- mongo/main.py
+++ mongo/main.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 
-from .extensions import db
+from .extensions import mongo_client
+from .db import create_doc, display_doc, display_id
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    status=db.command('serverStatus')
-    return str(status)
+    mdb = mongo_client.test
+    char1 = create_doc(mdb, 'Tiro', 10, 4, 5)
+    # found_chars = display_doc(mdb, 'Tiro')
+    found_chars = display_id(mdb, "5e56fc630000b90542314c86")
+    return str(found_chars)
