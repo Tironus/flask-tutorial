acf26c88e00faf78fe8126d7833e37fbe306158d
Tony <stephen.gionfriddo@gmail.com>
2020-02-26 14:31:10 -0700

    added mongodb client connection

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,31 +2,37 @@ import os
 
 from flask import Flask
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# connect to cloud mongodb
+client = MongoClient('mongodb+srv://tony:tonydb1@cluster0-tony-fffjh.mongodb.net/test?retryWrites=true&w=majority')
+db = client.admin
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+app.config['SECRET_KEY'] = 'dev'
+app.config['DATABASE'] = db
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-    return app
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+# a simple page that says hello
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
